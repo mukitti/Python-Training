fbb4c84333c6c2c8403f20de4c56b5338a3954ff
mukitti <mukitti@gmail.com>
2025-02-17 11:15:42 +0700

    Exam3-27.py

---
+++ Exam3-27.py
@@ -0,0 +1,11 @@
+# การใช้งานตัวดำเนินงานการรวจสอบสมาชิก
+
+cars = ["Honda", "Toyota", "BMW", "Benz", "Nissan", "GM", "Mazda"]
+
+my_car = "Ford"
+
+x = my_car in cars
+y = my_car not in cars
+
+print("ผลลัพธ์ my_car in cars :",x)
+print("ผลลัพธ์ my_car not in cars :",y)
